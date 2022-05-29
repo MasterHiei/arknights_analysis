47ecbf77e81fc3ca406c1d855f792685d21935d9
MasterHiei <ruhrg.lcx@outlook.com>
2022-05-30 04:23:26 +0800

    chore: update project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,35 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-import 'initializer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:window_manager/window_manager.dart';
+
+import 'core/providers.dart';
+import 'presentation/arknights_analysis.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(await initializeApp());
+  runApp(await _initializeApp());
+}
+
+Future<Widget> _initializeApp() async {
+  if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
+    await windowManager.ensureInitialized();
+    final options = WindowOptions(
+      size: const Size(1920, 1080),
+      center: true,
+      minimumSize: const Size(1280, 720),
+      skipTaskbar: false,
+      title: 'Arknights Analysis',
+    );
+    windowManager.waitUntilReadyToShow(options, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+  }
+
+  return ProviderScope(
+    overrides: await generateOverrides(),
+    child: const ArknightsAnalysis(),
+  );
 }
