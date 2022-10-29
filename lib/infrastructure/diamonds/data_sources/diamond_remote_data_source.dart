3aebcac67e44f959d2cce23a591772b5e3dca0fa
MasterHiei <ruhrg.lcx@outlook.com>
2022-10-29 18:39:07 +0800

    feat: create remote data source

---
+++ lib/infrastructure/diamonds/data_sources/diamond_remote_data_source.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/http.dart';
+
+import '../../../core/providers.dart';
+import '../dtos/diamond_response_dto.dart';
+
+part 'diamond_remote_data_source.g.dart';
+
+final diamondRemoteDataSourceProvider =
+    Provider.autoDispose<DiamondRemoteDataSource>(
+  (ref) => DiamondRemoteDataSource(ref.watch(akDioProvider)),
+);
+
+@RestApi()
+abstract class DiamondRemoteDataSource {
+  factory DiamondRemoteDataSource(Dio dio) = _DiamondRemoteDataSource;
+
+  @GET('/user/api/inquiry/diamond')
+  Future<DiamondResponseDto> fetch({
+    @Query('token') required String token,
+    @Query('page') required int page,
+    @Query('channelId') String? channelId,
+  });
+}
